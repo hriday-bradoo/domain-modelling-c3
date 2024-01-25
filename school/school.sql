0dafdf83b7656162e49bdae11fda5df3a767315b
Hriday Bradoo <hriday.bradoo@kellogg.northwestern.edu>
2024-01-25 04:16:06 +0000

    re commit to be safe

--- school/school.sql
+++ school/school.sql
@@ -15,5 +15,25 @@ CREATE TABLE students (
 );
 
 -- Create the rest of the tables
+-- Teacher table
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+-- course table
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+-- sections table
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
 
